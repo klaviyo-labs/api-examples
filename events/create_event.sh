a3199a2c1bc37d96640a3e3563d1d6a5c1280163
Michaela <michaela.fooksa@klaviyo.com>
2025-06-02 13:43:21 -0600

    Added create event example code

---
+++ events/create_event.sh
@@ -0,0 +1,40 @@
+curl --request POST \
+     --url https://a.klaviyo.com/api/events \
+     --header 'Authorization: Klaviyo-API-Key your-private-api-key' \
+     --header 'accept: application/vnd.api+json' \
+     --header 'content-type: application/vnd.api+json' \
+     --header 'revision: 2025-04-15' \
+     --data '
+{
+    "data": {
+        "type": "event",
+        "attributes": {
+            "properties": {
+                "Makeup type": "wedding",
+                "Makeup style": "natural",
+                "Appointment date": "2025-09-07"
+            },
+            "value": 100,
+            "value_currency": "USD",
+            "metric": {
+                "data": {
+                    "type": "metric",
+                    "attributes": {
+                        "name": "Booked makeup appointment"
+                    }
+                }
+            },
+            "profile": {
+                "data": {
+                    "type": "profile",
+                    "attributes": {
+                        "properties": {
+                            "email": "michaela.klaviyo@gmail.com"
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+'
