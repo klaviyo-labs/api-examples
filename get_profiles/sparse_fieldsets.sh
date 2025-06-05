5ea133b9f8e47fb81ab054101ee571e8c1a50d55
michaelafooksa <128404154+michaelafooksa@users.noreply.github.com>
2025-06-05 13:27:46 -0600

    Update sparse_fieldsets.sh

--- get_profiles/sparse_fieldsets.sh
+++ get_profiles/sparse_fieldsets.sh
@@ -1,3 +1,4 @@
+# sample cURL request to the Get Profiles endpoint to retrieve the email and country of all profiles in a Klaviyo account. This code is used in the Get profiles out of Klaviyo via API YouTube video: https://www.youtube.com/watch?v=bHXJlV7wP0Q.
 curl -g --request GET \
      --url 'https://a.klaviyo.com/api/profiles?fields[profile]=email,location.country' \
      --header 'Authorization: Klaviyo-API-Key your-private-api-key' \
