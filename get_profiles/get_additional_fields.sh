977c6d4b07afc5bc1850969ac8c467997f8e6f59
michaelafooksa <128404154+michaelafooksa@users.noreply.github.com>
2025-06-05 13:27:31 -0600

    Update get_additional_fields.sh

--- get_profiles/get_additional_fields.sh
+++ get_profiles/get_additional_fields.sh
@@ -1,3 +1,4 @@
+# sample cURL request to the Get Profiles endpoint to retrieve all profiles in a Klaviyo account, including subscription data and predictive analytics. This code is used in the Get profiles out of Klaviyo via API YouTube video: https://www.youtube.com/watch?v=bHXJlV7wP0Q.
 curl -g --request GET \
      --url 'https://a.klaviyo.com/api/profiles?additional-fields[profile]=subscriptions,predictive_analytics' \
      --header 'Authorization: Klaviyo-API-Key your-private-api-key' \
