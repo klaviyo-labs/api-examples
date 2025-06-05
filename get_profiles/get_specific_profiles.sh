0f97b0fb0b68e8219bf250ae2503686df72db3f6
michaelafooksa <128404154+michaelafooksa@users.noreply.github.com>
2025-06-05 13:26:45 -0600

    Update get_specific_profiles.sh

--- get_profiles/get_specific_profiles.sh
+++ get_profiles/get_specific_profiles.sh
@@ -1,3 +1,4 @@
+# sample cURL request to the Get Profiles endpoint to retrieve a specific profile from a Klaviyo account. This code is used in the Get profiles out of Klaviyo via API YouTube video: https://www.youtube.com/watch?v=bHXJlV7wP0Q.
 curl --request GET \
      --url 'https://a.klaviyo.com/api/profiles?filter=equals("email","michaela.klaviyo@gmail.com")' \
      --header 'Authorization: Klaviyo-API-Key your-private-api-key' \
