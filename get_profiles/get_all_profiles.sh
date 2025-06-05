4e4e05df46c5c50a51f066c7dc218693da4cc4e4
michaelafooksa <128404154+michaelafooksa@users.noreply.github.com>
2025-06-05 13:26:06 -0600

    Update get_all_profiles.sh

--- get_profiles/get_all_profiles.sh
+++ get_profiles/get_all_profiles.sh
@@ -1,3 +1,4 @@
+# sample cURL request to the Get Profiles endpoint to retrieve all profiles in a Klaviyo account. This code is used in the Get profiles out of Klaviyo via API YouTube video: https://www.youtube.com/watch?v=bHXJlV7wP0Q.
 curl --request GET \
      --url 'https://a.klaviyo.com/api/profiles' \
      --header 'Authorization: Klaviyo-API-Key your-private-api-key' \
