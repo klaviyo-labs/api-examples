6081ab30d64eb0f2ec55f8245a565d3c4a374b10
michaelafooksa <128404154+michaelafooksa@users.noreply.github.com>
2025-06-05 13:27:07 -0600

    Update get_group_of_profiles.sh

--- get_profiles/get_group_of_profiles.sh
+++ get_profiles/get_group_of_profiles.sh
@@ -1,3 +1,4 @@
+# sample cURL request to the Get Profiles endpoint to retrieve all profiles in a Klaviyo account that were suppressed after January 1, 2024. This code is used in the Get profiles out of Klaviyo via API YouTube video: https://www.youtube.com/watch?v=bHXJlV7wP0Q.
 curl --request GET \
      --url 'https://a.klaviyo.com/api/profiles?filter=less-or-equal(subscriptions.email.marketing.suppression.timestamp,2024-01-01T00:00:00)' \
      --header 'Authorization: Klaviyo-API-Key your-private-api-key' \
