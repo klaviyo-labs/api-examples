b52ecef2c92604f443a9b2b806898a43180b853d
michaelafooksa <128404154+michaelafooksa@users.noreply.github.com>
2025-06-05 13:33:00 -0600

    Update query_campaign_values.sh

--- reporting/query_campaign_values.sh
+++ reporting/query_campaign_values.sh
@@ -1,3 +1,4 @@
+# sample cURL request to the Query Campaign Values endpoint. This code is used in the Analyze marketing performance with reporting APIs YouTube video: https://www.youtube.com/watch?v=tM0wmuNnukU
 curl --request POST \
      --url https://a.klaviyo.com/api/campaign-values-reports \
      --header 'Authorization: Klaviyo-API-Key your-private-api-key' \
